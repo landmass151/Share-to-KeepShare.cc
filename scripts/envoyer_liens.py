c28d3844285193a11743f5f0fb2f968dcf7bd606
landmass151 <moonwalk_landmass151@slmails.com>
2026-09-03 09:51:12 +0200

    Update MAX_PAGES_SECURITE to 1000
    Increased the maximum number of pages for URLs containing '*' from 10 to 1000.

--- scripts/envoyer_liens.py
+++ scripts/envoyer_liens.py
@@ -23,7 +23,7 @@ DOSSIER_LOG = Path("log-url")
 BASE_URL_ENVOI = "https://keepshare.org/ldf6j5ti/"
 
 # Nombre maximal de pages lorsqu'une URL contient "*".
-MAX_PAGES_SECURITE = 10
+MAX_PAGES_SECURITE = 1000
 
 # Paramètres HTTP.
 TIMEOUT_HTTP = 30
